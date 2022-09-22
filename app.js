374b7093af86ffffd0237b2ee77b4a33bd7dd9c7
Ritesh sute <suteritesh@gmail.com>
2022-09-22 23:28:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,2 @@
+console.log('inti');
+ add()
---
+++ dom.js
@@ -0,0 +1,15 @@
+
+let header = document.getElementById('header-title');
+header.textContent = "New ITEM"
+
+let best = document.getElementsByTagName('title'); 
+best.textContent = 'best Item';
+
+let header2 = document.getElementById('main-header');
+header2.style.borderBottom = 'solid 3px #000';
+
+items = document.getElementsByClassName('title'); 
+items[0].style.color  = 'green';
+items[0].style.fontWeight = 'bold';
+
+
