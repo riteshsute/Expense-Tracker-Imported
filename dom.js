d7d0ac08bffd2122963d86b029734d9d7cc8c5b8
Ritesh sute <suteritesh@gmail.com>
2022-09-23 23:59:28 +0530

    addAndDelete

--- dom.js
+++ dom.js
@@ -90,3 +90,45 @@ newDiv.style.fontSize = '30px';
 let newDiv2 = document.createElement('div');
 
 itemlist.firstChild.textContent = 'HEllo';
+
+
+
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+
+
+form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem);
+
+function addItem(e){
+  e.preventDefault();
+
+  let newItem = document.getElementById('item').value;
+
+  let li = document.createElement('li');
+
+  li.className = 'list-group-item';
+  
+  li.appendChild(document.createTextNode(newItem));
+
+  let deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
+    }
+  }
+  
