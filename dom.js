7b8f1d27d841540f247f7818ea194e3f56244d3a
Ritesh sute <suteritesh@gmail.com>
2022-09-23 18:08:20 +0530

    DOM Manipulation trial

--- dom.js
+++ dom.js
@@ -1,47 +1,92 @@
 
-let header = document.getElementById('header-title');
-header.textContent = "New ITEM"
+//
+// let header = document.getElementById('header-title');
+// header.textContent = "New ITEM"
 
-let best = document.getElementsByTagName('title'); 
-best.textContent = 'best Item';
+// let best = document.getElementsByTagName('title'); 
+// best.textContent = 'best Item';
 
-let header2 = document.getElementById('main-header');
-header2.style.borderBottom = 'solid 3px #000';
+// let header2 = document.getElementById('main-header');
+// header2.style.borderBottom = 'solid 3px #000';
 
-let items = document.getElementsByClassName('title'); 
-items[0].style.color  = 'green';
-items[0].style.fontWeight = 'bold';
-
-
-let ok = document.getElementsByTagName('li');
-ok[4].textContent = 'Hello 5';
+// let items = document.getElementsByClassName('title'); 
+// items[0].style.color  = 'green';
+// items[0].style.fontWeight = 'bold';
 
 
-let addingid = document.getElementsByClassName('list-group-item');
-addingid[2].style.backgroundColor = "green";
-
-for( let i = 0; i < addingid.length; i++){
-    addingid[i].style.fontWeight = 'bold';
-}
+// let ok = document.getElementsByTagName('li');
+// ok[4].textContent = 'Hello 5';
 
 
-let secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
+// let addingid = document.getElementsByClassName('list-group-item');
+// addingid[2].style.backgroundColor = "green";
+
+// for( let i = 0; i < addingid.length; i++){
+//     addingid[i].style.fontWeight = 'bold';
+// }
 
 
-let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.hidden = 'hidden';
-
-let add = document.querySelectorAll('.list-group-item');
-add[1].style.color = 'white';
-
-odd = document.querySelectorAll('li:nth-child(odd)');
-
-for( let i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
 
 
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.hidden = 'hidden';
+
+// let add = document.querySelectorAll('.list-group-item');
+// add[1].style.color = 'white';
+
+// odd = document.querySelectorAll('li:nth-child(odd)');
+
+// for( let i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+// console.log('get element by name');
+// dom = document.querySelector('.list-group-item')
 
 
+const itemlist = document.querySelector('#items');
+itemlist.parentElement.style.backgroundColor = '#f4f4f4';
 
+
+itemlist.lastElementChild.textContent = 'this is item 5';
+
+itemlist.lastChild.textContent = 'last child created';
+
+itemlist.firstElementChild.style.color = '#EC6649';
+
+itemlist.firstChild.textContent = 'this is first child';
+
+itemlist.nextSibling.textContent = 'nothing here';
+
+// we don't have any sibling element
+
+itemlist.previousElementSibling.style.color = '#276356';
+
+itemlist.previousElementSibling.style.backgroundColor = '#7C88CC';
+
+itemlist.nextSibling = 'display: none';
+
+
+let newDiv = document.createElement('div');
+
+newDiv.className = 'hello';
+
+newDiv.id = 'hello1';
+
+newDiv.setAttribute('title', 'hello div');
+
+let newDivText = document.createTextNode(' HEllo ');
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+newDiv.style.fontSize = '30px';
+
+
+let newDiv2 = document.createElement('div');
+
+itemlist.firstChild.textContent = 'HEllo';
