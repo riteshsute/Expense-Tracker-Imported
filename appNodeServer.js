0932f53a42d11f60e3f1b5731a72d150bccbc87e
Ritesh sute <suteritesh@gmail.com>
2022-12-02 22:30:32 +0530

    RoiutingNodeServerCode.js

--- appNodeServer.js
+++ appNodeServer.js
@@ -1,55 +1,10 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
-const server = http.createServer((req, res)=>{
-const url = req.url;
-const method = req.method
+const routes = require('./routes');
 
-   if(url === '/'){
-      fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
-         if(err){
-            console.log(err)
-         }
-         res.write('<html>');
-         res.write('<head><title>My First Page</title></head>');
-         res.write(`<body>${data}</body>`);
-         res.write(
-            `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>`
-            );
-         res.write('</html>');
-         return res.end()
-      })
-   }
-   else if (url === "/message" && method === "POST"){
-         const body = [];
-         req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-         });
+const server = http.createServer(routes);
 
-         return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-               if(err){
-                  console.log(err)
-               }
-               res.statusCode = 302;
-               res.setHeader('Location', '/');
-               return res.end()
-            
-            });
-         })
-      } 
-      else {
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html>');
-      res.write('<head><title>My First Page</title></head>');
-      res.write('<body><h1>Hello From My Node.js Server!</h1></body>');
-      res.write('</html>');
-      res.end()
-      }
-   });
+server.listen(4000);
 
-      server.listen(4000);
+console.log(server)
+// routed to routes.js file 
--- failedServer.js
+++ failedServer.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-   res.write("Ritesh sute")
+   res.write("Ritesh")
    res.end()
 });
 
